6458d3471e2dabb8cdef7c6ded2288ef60a2ad06
ghostfighter50 <ghostfighter5015@gmail.com>
2020-10-20 15:21:04 +0200

    d

--- events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = (client) => {
-    console.log(`[+] Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
-    
-  }
