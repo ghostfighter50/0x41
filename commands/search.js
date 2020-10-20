b3060adeae9e7e156469121798260dd494420fa2
ghostfighter50 <ghostfighter5015@gmail.com>
2020-10-20 15:21:15 +0200

    s

---
+++ commands/search.js
@@ -0,0 +1,32 @@
+exports.run = async  (client, message) => {
+    let args = message.content.slice(4).split(' ')
+const serp = require("serp");
+const Discord = require("discord.js")
+var options = {
+  host : "google.com",
+  qs : {
+    q : args[2],
+    filter : 0,
+    pws : 0
+  },
+  num : 20
+};
+const waitembed = new Discord.MessageEmbed()
+.setThumbnail(message.author.avatarURL)
+.setURL("https://discord.gg/s6aFpGq")
+.setFooter("Hack-Harder : https://discord.gg/s6aFpGq")
+.setTitle(`🔁 I'm preparing your search...`) 
+.setColor(0x00AE86);
+message.channel.send(waitembed)
+const links = await serp.search(options)
+const embed = new Discord.MessageEmbed()
+.setThumbnail(message.author.avatarURL)
+.setURL("https://discord.gg/s6aFpGq")
+.setFooter("Hack-Harder : https://discord.gg/s6aFpGq")
+.setTitle(`Google search results`) 
+.setColor(0x00AE86);
+
+links.forEach(link =>{ embed.addField( link.title,` (${link.url})`, false)
+})
+message.channel.send(embed)
+}
---
+++ events/ready.js
@@ -0,0 +1,4 @@
+module.exports = (client) => {
+    console.log(`[+] Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
+    
+  }
