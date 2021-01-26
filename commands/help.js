85a793e6c9a4519a64d9a94a19141001bb1eecb7
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-26 16:12:45 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ exports.run = async  (client, message) => {
     const Discord = require("discord.js")
     const embed = new Discord.MessageEmbed()
     .setThumbnail(message.author.avatarURL)
-    .addField("\n\n** 📚 Informations **","`() = options`  `<> = required`, `< | > = possible values`   `@user = mention`"
+    .addField("\n\n** 📚 Informations **","`() = options`  `<> = required`, `< | > = possible values`   `@user = mention`")
     .addField("** 🔨 Moderation**","`tempmute <@user> (reason)`   `unmute <@user> (reason)`   `mute <@user> (reason)`   `kick <@user> (reason)`   `kick <@user> (reason)`   `unban <user id> (reason)`   `clear <number>`  ", false)
     .addField("** ⚙️ Utilities**","`exec <command>`   `stats`   `userinfo <@user>`   `serverinfo`   `ping `   `hastebin <value>`   `encode <base64|hex> <value>`   `decode <base64|hex> <value>`   `search <keyword>`  ",false)
     .addField("** 📊 Points**","`give <user> <amount>`   `remove <user> <amount>`   `nukepoints`   `levels`   `updateroles`   `points`   `verify <challenge-name> <flag> `   `leaderboard `  ", false)
