f7dbd89c7e82a7a4a0734c0f606b23621fcefa85
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-26 14:48:10 +0100

    Update levels.js

--- commands/levels.js
+++ commands/levels.js
@@ -2,12 +2,10 @@ exports.run = async  (client, message) => {
     const Discord = require("discord.js")
     const embed = new Discord.MessageEmbed()
     .setThumbnail(message.author.avatarURL)
-    .setURL("https://discord.gg/pU2JHgG5Mj")
     .addFields(
-    {name: "1️⃣ Débutant", value : "20 Points"},
-    {name: "2️⃣ Intermediaire", value : "50 Points"},
+    {name: "1️⃣ Begginer", value : "20 Points"},
+    {name: "2️⃣ Intermediate", value : "50 Points"},
     {name: "3️⃣ Expert", value : "100 Points"})
-    .setFooter("Dystopia : https://discord.gg/pU2JHgG5Mj")
     .setTitle(`📖 Help `) 
     .setColor(0x00AE86)   
 
