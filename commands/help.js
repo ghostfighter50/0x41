83c96281cc4883624a342d7e8b624bcad1e422f2
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-26 15:48:20 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ exports.run = async  (client, message) => {
     const Discord = require("discord.js")
     const embed = new Discord.MessageEmbed()
     .setThumbnail(message.author.avatarURL)
-    .addField("**Points"**,"`sudo give <user> <amount>` `sudo remove <user> <amount>` `sudo nukepoints` `sudo levels` `sudo updateroles` `sudo points` `sudo verify <challenge-name> <flag> ` `sudo leaderboard `", false)
+    .addField("**Points**","`sudo give <user> <amount>` `sudo remove <user> <amount>` `sudo nukepoints` `sudo levels` `sudo updateroles` `sudo points` `sudo verify <challenge-name> <flag> ` `sudo leaderboard `", false)
     .addField("**Moderation"**,"`sudo tempmute <@user> (reason)` `sudo unmute <@user> (reason)` `sudo mute <@user> (reason)` `sudo kick <@user> (reason)` `sudo kick <@user> (reason)` `sudo unban <user id> (reason)` `sudo clear <number>`", false)
     .addField("**Utilities**","`sudo exec <command>` `sudo stats` `sudo userinfo <@user>` `sudo serverinfo` `sudo ping ` `sudo hastebin <value>` `sudo encode <base64|hex> <value>` `sudo decode <base64|hex> <value>` `sudo search <keyword>`",false)
     .setTitle(`📖 Help `) 
