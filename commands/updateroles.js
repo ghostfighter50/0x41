e2df6d4b2bc83008dfe0dae7458c097174de123f
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-26 14:57:26 +0100

    Update updateroles.js

--- commands/updateroles.js
+++ commands/updateroles.js
@@ -1,35 +1,34 @@
 exports.run = async (client, message) => {
 const Discord = require('discord.js');
+    
 const verified = new Discord.MessageEmbed()
 .setThumbnail(message.author.avatarURL)
-.setURL("https://discord.gg/pU2JHgG5Mj")
-.setFooter("Dystopia : https://discord.gg/pU2JHgG5Mj")
 .setTitle(`✅ Roles updated ! `) 
 .setColor(0x00AE86);
+    
 let embed = new Discord.MessageEmbed()
 .setTitle("❌ Not enough points ")
 .setDescription("Don't give up, you can make those challenges !")
 .setThumbnail(message.guild.iconURL)
 .setColor(0x00AE86)
-.setURL("https://discord.gg/pU2JHgG5Mj")
-.setFooter("Dystopia : https://discord.gg/pU2JHgG5Mj")
+
 let user = message.author
 let userPoints = client.points.get(`${message.guild.id}-${user.id}`, "points");
 let member = message.member;
 if (userPoints >= "100") {
     
-   let testedRole = message.guild.roles.cache.get('782268863903432734');
+   let testedRole = message.guild.roles.cache.get('803603102330126366');
    let testedUser = member
-   let removerole =  message.guild.roles.cache.get('782268864326008873');
+   let removerole =  message.guild.roles.cache.get('803603102653218837');
    testedUser.roles.remove(removerole)
    testedUser.roles.add(testedRole);
    message.channel.send(verified);
 }
 else if (userPoints >= "50") {
     
-   let testedRole = message.guild.roles.cache.get('782268864326008873');
+   let testedRole = message.guild.roles.cache.get('803603102653218837');
    let testedUser = member
-   let removerole =  message.guild.roles.cache.get('782268865048215583');
+   let removerole =  message.guild.roles.cache.get('803603103961841674');
    testedUser.roles.remove(removerole)
    
 
@@ -38,7 +37,7 @@ else if (userPoints >= "50") {
 }
 else if (userPoints >= "20") {
     
-        let testedRole = message.guild.roles.cache.get('782268865048215583');
+        let testedRole = message.guild.roles.cache.get('803603103961841674');
         let testedUser = member
         
     
@@ -47,6 +46,10 @@ else if (userPoints >= "20") {
       }
    
     
-    else return message.channel.send(embed)
-    }    
+else {
+    let removerole =  message.guild.roles.cache.get('803603102330126366');
+   testedUser.roles.remove(removerole)
+    message.channel.send(embed)
+    }
+}    
     
