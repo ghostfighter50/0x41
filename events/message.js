5b0036428735592c2f140ecfaf8204449a9e00bd
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-26 18:47:48 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -7,7 +7,11 @@ module.exports = (client, message) => {
     const command = args.shift().toLowerCase();
   
     const cmd = client.commands.get(command);
-
+    
+    if (message.mentions.has(client.user.id)) {
+        message.channel.reply("my prefix is `sudo`, try `sudo help` to see all my commands.");
+    };
+    
     if (message.guild) {
         client.points.ensure(`${message.guild.id}-${message.author.id}`, {
           user: message.author.id,
