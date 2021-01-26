1fa3ba48ab06efe07fed1fbefa3f2519c1055490
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-26 18:51:28 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,4 @@
+module.exports = (member) =>{
 const Discord = require("discord.js")
 const embed = new Discord.MessageEmbed()
         .setColor('#0099ff')
@@ -5,4 +6,5 @@ const embed = new Discord.MessageEmbed()
         .addField('', member.nickname)
         .setImage(member.user.avatarURL)
 
-    member.guild.channels.get('605000838573850635').send(exampleEmbed);
+    member.guild.channels.get('803603228498329652').send(embed);
+}
