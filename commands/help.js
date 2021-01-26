391fa8574d68e2126dd3f4854085e9c9357be4fa
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-26 16:06:37 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,10 +2,12 @@ exports.run = async  (client, message) => {
     const Discord = require("discord.js")
     const embed = new Discord.MessageEmbed()
     .setThumbnail(message.author.avatarURL)
-    .addField("** 🔨 Moderation**","`tempmute <@user> (reason)` `unmute <@user> (reason)` `mute <@user> (reason)` `kick <@user> (reason)` `kick <@user> (reason)` `unban <user id> (reason)` `clear <number>`", false)
-    .addField("** ⚙️ Utilities**","`exec <command>` `stats` `userinfo <@user>` `serverinfo` `ping ` `hastebin <value>` `encode <base64|hex> <value>` `decode <base64|hex> <value>` `search <keyword>`",false)
-    .addField("** 📊 Points**","`give <user> <amount>` `remove <user> <amount>` `nukepoints` `levels` `updateroles` `points` `verify <challenge-name> <flag> ` `leaderboard `", false)
-    .setTitle(`📖 Help `) 
+    .addField("\n\n** 📚 Informations **","`() = options`  `<> = required`, `< | > = possible values`   `@user = mention`"
+    .addField("** 🔨 Moderation**","`tempmute <@user> (reason)`   `unmute <@user> (reason)`   `mute <@user> (reason)`   `kick <@user> (reason)`   `kick <@user> (reason)`   `unban <user id> (reason)`   `clear <number>`  ", false)
+    .addField("** ⚙️ Utilities**","`exec <command>`   `stats`   `userinfo <@user>`   `serverinfo`   `ping `   `hastebin <value>`   `encode <base64|hex> <value>`   `decode <base64|hex> <value>`   `search <keyword>`  ",false)
+    .addField("** 📊 Points**","`give <user> <amount>`   `remove <user> <amount>`   `nukepoints`   `levels`   `updateroles`   `points`   `verify <challenge-name> <flag> `   `leaderboard `  ", false)
+    .setTitle(`📖 Help `)
+    .setFooter(`Made by Ghostfighter50`)
     .setColor(0x00AE86);    
 
     message.channel.send(embed)
