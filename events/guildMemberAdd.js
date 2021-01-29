7450b5082f2defcdaf18c63b1f44ccd5dc8d77d6
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-29 11:20:01 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -25,7 +25,7 @@ module.exports = async (client, member) => {
                             .then(question2 => {
                                 question2.react("1️⃣")
                                 question2.react("2️⃣")
-                                question2.awaitReactions((reaction) => (reaction.emoji.name == '1️⃣' || reaction.emoji.name == '2️⃣' && reaction.count > 2), {
+                                question2.awaitReactions((reaction, user) => (reaction.emoji.name == '1️⃣' || reaction.emoji.name == '2️⃣'  && user.id == question2.author.id ), {
                                         max: 1,
                                         time: 5000
                                     })
@@ -39,7 +39,7 @@ module.exports = async (client, member) => {
                                                 .then(question3 => {
                                                     question3.react("1️⃣")
                                                     question3.react("2️⃣")
-                                                    question3.awaitReactions((reaction) => (reaction.emoji.name == '1️⃣' || reaction.emoji.name == '2️⃣' && reaction.count > 2), {
+                                                    question3.awaitReactions((reaction, user) => (reaction.emoji.name == '1️⃣' || reaction.emoji.name == '2️⃣'  && user.id == question3.author.id ), {
                                                             max: 1,
                                                             time: 5000
                                                         })
