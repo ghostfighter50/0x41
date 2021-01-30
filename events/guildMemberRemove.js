ec7b8e14e3bf143643ffefa7ecebed55206bcb86
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-30 17:12:55 +0100

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,1 +1,15 @@
 
+module.exports = async (client, member) => {
+
+    const Discord = require("discord.js")
+    let chatChannel = member.guild.channels.cache.find(c => c.name == "💬•off-topic")
+
+ 
+    let embed = new Discord.MessageEmbed()
+        .setTitle(`❌ ${member.user.username} left...`)
+        .setColor(0x00AE86)
+        .setThumbnail(member.avatarURL)
+
+    await channel.send(embed)
+   
+}
