4b815cc11a616ce23577ecbafba3c37e30758f7e
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-30 16:51:03 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,10 @@
 module.exports = async (client, member) => {
 
     const Discord = require("discord.js")
-
+    let chatChannel = member.guild.channels.cache.find(c => c.name == "💬•off-topic")
     let channel = member.guild.channels.cache.find(c => c.name == "skid-test")
     let role = member.guild.roles.cache.find(role => role.id == 803625942647570493);
-
+    let delrole = member.guild.roles.cache.find(role => role.id == 803603107002974228);
 
     let embed = new Discord.MessageEmbed()
         .setTitle(`New Member : ${member.user.username}`)
@@ -12,18 +12,34 @@ module.exports = async (client, member) => {
         .setColor(0x00AE86)
         .setThumbnail(member.avatarURL)
 
-    await channel.send(embed)
+    let verifiedembed = new Discord.MessageEmbed()
+        .setTitle(`✅ ${member.user.username} Passed the test`)
+        .setColor(0x00AE86)
+        .setThumbnail(member.avatarURL)
 
+    let deniedembed = new Discord.MessageEmbed()
+        .setTitle(`❌ ${member.user.username} failed the test`)
+        .setColor(0x00AE86)
+        .setThumbnail(member.avatarURL)
+
+    await channel.send(embed)
+    await channel.send(verifiedembed)
+    await chatChannel.send(verifiedembed)
 
 
     let question1 = await member.send(new Discord.MessageEmbed().setDescription("Decode this string : `WW91IHBhc3NlZCB0aGUgZmlyc3QgcXVlc3Rpb24gISBQcmVwYXJlIGZvciB0aGUgc2Vjb25kIG9uZS4=`").setColor(0x00AE86))
 
 
-    question1.channel.awaitMessages(response => response.content, { max: 1 })
+    question1.channel.awaitMessages(response => response.content, {
+            max: 1
+        })
 
         .then(async collected => {
+
             if (collected.first().content == "You passed the first question ! Prepare for the second one.") {
-                await member.send('Correct Answer,  1/3')
+
+                await member.send('✅ Correct Answer,  1/3')
+
                 let question2 = await member.send(new Discord.MessageEmbed().setDescription("What does `boot` mean ? \n\n1️⃣ : Ddos attack\n2️⃣ : starting a computer").setColor(0x00AE86))
 
                 const Filter1 = (reaction, user) => user.id !== question2.author.id;
@@ -31,15 +47,15 @@ module.exports = async (client, member) => {
                 await question2.react("1️⃣")
                 await question2.react("2️⃣")
 
-                question2.awaitReactions(Filter1, { max: 1 })
+                question2.awaitReactions(Filter1, {
+                        max: 1
+                    })
 
                     .then(async collected => {
-                        console.log("test")
-                        console.log(collected.first().emoji.name, "ok")
-                    
+
                         if (collected.first().emoji.name == '2️⃣') {
 
-                            await member.send('Correct Answer,  2/3')
+                            await member.send('✅ Correct Answer,  2/3')
 
                             let question3 = await member.send(new Discord.MessageEmbed().setDescription("What does the `ls` command does on a UNIX system ?\n\n1️⃣ : List files\n2️⃣ : navigate into a directory").setColor(0x00AE86))
 
@@ -47,35 +63,43 @@ module.exports = async (client, member) => {
 
                             await question3.react("1️⃣")
                             await question3.react("2️⃣")
-                            
+
                             question3.awaitReactions(Filter2, {
                                     max: 1
                                 })
 
                                 .then(async collected1 => {
 
-                                    console.log(collected1.first().emoji.name)
                                     if (collected1.first().emoji.name == '1️⃣') {
 
-                                        await member.send("Congratulations, you passed the test ! 3/3")
+                                        await member.send("✅ Congratulations, you passed the test ! 3/3")
 
                                         await member.roles.add(role)
+                                        await member.roles.remove(delrole)
 
                                     } else if (collected1.first().emoji.name == '2️⃣') {
 
-                                        await member.send('Incorrect Answer, you\'re going to be kicked 2/3');
+                                        await member.send('❌ Incorrect Answer, you\'re going to be kicked 2/3');
+                                        channel.send(deniedembed)
+                                        member.kick()
 
                                     }
                                 })
 
                         } else if (collected.first().emoji.name == '1️⃣') {
 
-                            member.send('Incorrect Answer, you\'re going to be kicked 1/3');
+                            member.send('❌ Incorrect Answer, you\'re going to be kicked 1/3');
+                            channel.send(deniedembed)
+                            member.kick()
                         }
                     })
 
-            }
+            } else {
 
+                member.send('❌ Incorrect Answer, you\'re going to be kicked 0/3');
+                channel.send(deniedembed)
+                member.kick()
+            }
         })
 
 }
