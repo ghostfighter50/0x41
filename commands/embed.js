1b706d2207c220234ab08eceb77953209ba919dc
ghostfighter50 <ghostfighter5015@gmail.com>
2021-01-31 10:32:59 +0100

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {       
     let content = args.join(" ")
-    let embed = new MessageEmbed()
+    let embed = new Discord.MessageEmbed()
       .setDescription(content)
     await message.channel.send(embed)
     await message.delete()
