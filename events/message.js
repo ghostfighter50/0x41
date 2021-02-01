2b195c6636e481c0cbef0f7bc50cb15abbec9da8
ghostfighter50 <ghostfighter5015@gmail.com>
2021-02-01 18:50:15 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -15,6 +15,7 @@ module.exports = (client, message) => {
     if (message.guild) {
         if(client.points.has(`${message.guild.id}-${message.author.id}`, 0) == true ) console.log("ok")
         else {
+         console.log("not ok")
          client.points.ensure(`${message.guild.id}-${message.author.id}`, {
           user: message.author.id,
           guild: message.guild.id,
