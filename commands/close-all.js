4e7f267a1270c1446d265b2f9a010822e11cfd10
ghostfighter50 <ghostfighter5015@gmail.com>
2021-02-01 07:58:22 +0100

    Update close-all.js

--- commands/close-all.js
+++ commands/close-all.js
@@ -4,7 +4,7 @@ exports.run = async (client, message) => {
 
 	if (!message.member.hasPermission("ADMINISTRATOR"))
 		return message.reply("❌ you can't use this command.");
-   message.guild.channels.forEach(c => {
+   message.guild.channels.cache.forEach(c => {
     if(c.name.startWith("ticket-")) return c.delete()
 })
 }
