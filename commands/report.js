f7be73f45cf5504041fb5ef78ea6bc520b4ad22e
ghostfighter50 <ghostfighter5015@gmail.com>
2021-02-02 15:42:52 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -2,13 +2,14 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
 let channel = message.guild.channels.cache.find(c => c.name == "logs")
-let TargetUser = message.mentions.first()
+let TargetUser = message.mentions.members.first()
 let reason = args[1]
 if(!TargetUser) return message.reply("❌ specify a user !")
 if(!reason) return message.reply("❌ enter a reason !")
 let embed = new Discord.MessageEmbed
-  .setTitle("New Report by " + message.author.toString()")
+  .setTitle("New Report by " + message.author.toString())
   .setDescription(`Reported user : ${TargetUser.toString()}\n\nReason : ${reason}`)
   .setColor(0x00AE86)
  channel.send(embed)
+message.channel.send(new Discord.MessageEmbed.setDescription(`✅ ${TargetUser.toString()} was reported by ${message.author.toString()} `
 }
