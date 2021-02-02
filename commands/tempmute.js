6a7e4b8e3c451b63149796ac8d7007ed68fc2c9a
ghostfighter50 <ghostfighter5015@gmail.com>
2021-02-02 20:23:57 +0100

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 const ms = require("ms");
 
 exports.run = async (client, message, args) => {
-    
-
+    let reason = args[3]
+    if (reason == undefined) reason = '❌ No reason Supplied';
     let tomute = message.mentions.users.first() || message.guild.members.cache.get(args[2]);
     if(!tomute) return message.reply("❌ Couldn't find that user.");
     if(message.author.id === message.mentions.users.first()) return message.reply("❌ You can't mute yourself:facepalm:");
@@ -35,6 +35,7 @@ exports.run = async (client, message, args) => {
     .addField('Action:', 'Temp Mute')
     .addField('User:', `${tomute.username}#${tomute.discriminator} (${tomute.id})`)
     .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason :', reason)
     .addField('Length', mutetime +" sec")
     .setColor(0x00AE86)
     message.channel.send({embed});
