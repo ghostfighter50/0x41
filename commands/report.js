5df8ce053f0218ad6fb39c4862803cecabc9a40c
ghostfighter50 <ghostfighter5015@gmail.com>
2021-02-23 16:46:50 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
 let channel = message.guild.channels.cache.find(c => c.name == "logs")
 let TargetUser = message.mentions.members.first()
-let reason = args[1]
+let reason = args.slice(1)
 if(!TargetUser) return message.reply("❌ specify a user !")
 if(!reason) return message.reply("❌ enter a reason !")
 let embed = new Discord.MessageEmbed()
