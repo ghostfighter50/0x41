3f0790b1a22eb1249f347d1de0eb8155ac96e434
ghostfighter50 <ghostfighter5015@gmail.com>
2021-02-25 19:28:04 +0000

    Update thm.js

--- commands/thm.js
+++ commands/thm.js
@@ -1,17 +1,26 @@
 
 const Discord = require("discord.js");
 
-exports.run = async (client, message, args) => {       
+exports.run = async (client, message, args) => {      
+    function imageExists(image_url){
+
+    var http = new XMLHttpRequest();
+
+    http.open('HEAD', image_url, false);
+    http.send();
+    if(http.status != 403) return true
+    else return false
+
+}
     let user = args[0]
     let embed = new Discord.MessageEmbed()
    .setColor(0x00AE86)
    .setImage(`https://tryhackme-badges.s3.amazonaws.com/${args[0]}.png`)
    .setFooter("You might want to reload the image in your profile")
-    try {
+    if(imageExists == true) {
     await message.channel.send(embed)
     await message.delete()
-    }
-    catch {
+    } else {
     await message.reply(`:x: Username ${args[0]} not found`)
     await message.delete()
     }
