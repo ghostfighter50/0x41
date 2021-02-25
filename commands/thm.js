e5c3fd40b7554a5c3e48c50efba1186d535ec807
ghostfighter50 <ghostfighter5015@gmail.com>
2021-02-25 19:18:28 +0000

    Update thm.js

--- commands/thm.js
+++ commands/thm.js
@@ -1,1 +1,18 @@
 
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {       
+    let user = args[0]
+    let embed = new Discord.MessageEmbed()
+   .setColor(0x00AE86)
+   .setThumbnail(`https://tryhackme-badges.s3.amazonaws.com/${args[0]}.png`)
+   .setFooter("You might want to reload the image in your profile")
+    try {
+    await message.channel.send(embed)
+    await message.delete()
+    }
+    catch {
+    await message.reply(`:x: Username ${args[0]} not found`)
+    await message.delete()
+    }
+}
