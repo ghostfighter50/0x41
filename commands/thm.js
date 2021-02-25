9ee52e411efd9fd3886ef389d953b4589ab90442
ghostfighter50 <ghostfighter5015@gmail.com>
2021-02-25 21:21:50 +0000

    Update thm.js

--- commands/thm.js
+++ commands/thm.js
@@ -10,18 +10,14 @@ exports.run = async (client, message, args) => {
     let embed = new Discord.MessageEmbed()
    .setColor(0x00AE86)
    .setImage(`https://tryhackme-badges.s3.amazonaws.com/${args[0]}.png`)
+   .setDescription("If there's not any images, the requested user profile is incorrect !")
    .setFooter("You might want to reload the image in your profile")
     
-    
- https.request(`https://tryhackme-badges.s3.amazonaws.com/${args[0]}.png`, async res => {
-  if(res.statusCode == 200) {
+
     await message.channel.send(embed)
     await message.delete()
-    } 
-  res.on("error",async () => {
-    await message.reply(`:x: Username ${args[0]} not found`)
-    await message.delete()  
-  })
+    
+  
 })
 
 }
